38c161667f790ce01e0793c93bbc80c83a221c7a
EricElMO <102978164+EricElMO@users.noreply.github.com>
2022-06-16 17:15:14 +0200

    UI Implemented

---
+++ Assets/Scripts/Class.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Clases de los personajes
+// Sujeto a cambios
+// Guerrero, Mago, Arquero, Pícaro
+
+public class ClassCharacter : MonoBehaviour
+{
+    public enum typeCharacter
+    {
+        Warrior,
+        Mage, 
+        Archer,
+        Rogue
+    }
+}
